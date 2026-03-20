cedb4030540516e28ba01a7101c54c28054c1b3e
abdirahmanmohamud510 <your-abdirahmanmohamud510@gmail.com>
2026-03-21 00:11:42 +0300

    added show password toggle

--- src/views/public/js/login.js
+++ src/views/public/js/login.js
@@ -1,3 +1,14 @@
+// PASSWORD HIDE SHOW TOGGLE
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+togglePassword.addEventListener("click", () => {
+  const isPassword = passwordInput.type === "password";
+  
+  passwordInput.type = isPassword ? "text" : "password";
+  togglePassword.classList.toggle("fa-eye");
+  togglePassword.classList.toggle("fa-eye-slash");
+});
 // LOGIN SUBMITION
 const loginForm = document.getElementById("login-form");
 
